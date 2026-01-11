5e96e92f101e4a3bb46f4c4fca9d88f91b1a5698
Rafael <12767@holbertonstudents.com>
2026-01-11 15:13:41 +0000

    Add pandas tasks 0 and 1

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Create DataFrame from NumPy array."""
+
+import pandas as pd
+
+
+def from_numpy(array):
+    """Create DataFrame with columns A, B, C, ..."""
+    num_cols = array.shape[1] if len(array.shape) > 1 else 1
+    columns = [chr(65 + i) for i in range(num_cols)]
+    return pd.DataFrame(array, columns=columns)
---
+++ pipeline/pandas/1-main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+df = __import__('1-from_dictionary').df
+print(df)
