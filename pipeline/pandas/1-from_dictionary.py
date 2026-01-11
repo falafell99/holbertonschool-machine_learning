eb0dba86dfd947d2ed38ffd7f68db74ac32d3111
Rafael <12767@holbertonstudents.com>
2026-01-11 16:02:40 +0000

    Task 1: Create DataFrame from dictionary

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -3,12 +3,11 @@
 
 import pandas as pd
 
-
 # Create dictionary with data
 data = {
     'First': [0.0, 0.5, 1.0, 1.5],
     'Second': ['one', 'two', 'three', 'four']
 }
 
-# Create DataFrame from dictionary
+# Create DataFrame from dictionary with row labels
 df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
