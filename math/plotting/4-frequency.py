c29c8aa3cb027b9b90b813e60b194740de896653
Rafael <12767@holbertonstudents.com>
2026-01-16 12:18:00 +0000

    Task 4: Fix histogram with correct bins and edges

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Plot histogram of student grades."""
+"""Histogram of student grades."""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,8 +10,20 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+    # Create bins from 0 to 100 with step 10
+    bins = np.arange(0, 101, 10)
+
+    # Plot histogram with black edges
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    # Set labels and title
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+
+    # Set x-axis limits
+    plt.xlim(0, 100)
+
+    # Show plot
     plt.show()
