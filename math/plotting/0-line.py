ba86d54158af91d1903e519a288f1d12dc9565ec
Rafael <12767@holbertonstudents.com>
2026-01-16 12:15:25 +0000

    Try different 0-line implementations

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    y = np.arange(0, 11) ** 3
+    x = np.linspace(0, 10, 11)
+    y = x ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(y, 'r-')
+    plt.plot(x, y, 'r-', linewidth=2)
     plt.xlim(0, 10)
     plt.show()
