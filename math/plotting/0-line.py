6452a161b2a2d13029b75a0aa23505dd10b5c004
Rafael <12767@holbertonstudents.com>
2026-01-21 12:42:56 +0000

    Fix task 0: Simple red line plot

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    x = np.linspace(0, 10, 11)
-    y = x ** 3
+    y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, 'r-', linewidth=2)
+    plt.plot(y, 'r')
     plt.xlim(0, 10)
     plt.show()
