dc2e9bba51487e8b345ef43d3d271da7418a8ab9
Rafael <12767@holbertonstudents.com>
2026-02-02 15:00:14 +0000

    Task 11: Transpose numpy array using .T attribute

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Transpose numpy array."""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Transpose numpy array.
+
+    Args:
+        matrix: numpy.ndarray
+
+    Returns:
+        Transposed numpy array
+    """
+    return matrix.T
