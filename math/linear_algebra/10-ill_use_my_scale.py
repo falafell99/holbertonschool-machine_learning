6a4d6440981f4913f856624f171415b224eb1f97
Rafael <12767@holbertonstudents.com>
2026-02-02 14:59:52 +0000

    Task 10: Get numpy array shape using .shape attribute

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Get shape of numpy array."""
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """Return shape of numpy array.
+
+    Args:
+        matrix: numpy.ndarray
+
+    Returns:
+        Tuple representing array dimensions
+    """
+    return matrix.shape
