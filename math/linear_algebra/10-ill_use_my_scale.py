3a995ce24859c3bc0ed64fbfc8b42d5b6aae6258
Rafael <12767@holbertonstudents.com>
2026-02-02 15:01:58 +0000

    Fix tasks 10-11: remove import statements from files

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Get shape of numpy array."""
 
-import numpy as np
-
 
 def np_shape(matrix):
     """Return shape of numpy array.
-
+    
     Args:
         matrix: numpy.ndarray
-
+        
     Returns:
         Tuple representing array dimensions
     """
--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Transpose numpy array."""
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """Transpose numpy array.
-
+    
     Args:
         matrix: numpy.ndarray
-
+        
     Returns:
         Transposed numpy array
     """
