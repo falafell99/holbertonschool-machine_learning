5f0966d512db909d595691bc2ba41c25a87f0cde
Rafael <12767@holbertonstudents.com>
2026-02-14 23:14:50 +0000

    Fix histogram match and pycodestyle

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-import numpy as np
+"""Module to plot a histogram"""
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def frequency():
     """Plots a histogram of student scores for a project"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+
     plt.figure(figsize=(6.4, 4.8))
 
-    # Рисуем гистограмму
-    # bins=range(0, 101, 10) создает интервалы [0-10, 10-20, ..., 90-100]
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    # Устанавливаем лимиты и деления оси X
-    plt.xlim(0, 100)
-    plt.xticks(range(0, 101, 10))
-
-    # Добавляем подписи и заголовок
+    plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    plt.title('Project A')
+
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.xticks(range(0, 101, 10))
 
     plt.show()
