b0b1542547a3e8bda58f0f852b9e5b38debf40e0
Rafael <12767@holbertonstudents.com>
2026-02-14 22:59:44 +0000

    Fix precision and pycodestyle in Poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,17 +6,7 @@ class Poisson:
     """Represents a Poisson distribution."""
 
     def __init__(self, data=None, lambtha=1.):
-        """Initialize Poisson distribution.
-
-        Args:
-            data: List of data to estimate the distribution
-            lambtha: Expected number of occurrences in a given time frame
-
-        Raises:
-            ValueError: If lambtha is not positive or data has less than 2
-                       points
-            TypeError: If data is not a list
-        """
+        """Initialize Poisson distribution."""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -26,49 +16,35 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data)) / float(len(data))
+            self.lambtha = float(sum(data)) / len(data)
 
     def pmf(self, k):
-        """Calculate PMF value for k successes.
-
-        Args:
-            k: Number of successes
-
-        Returns:
-            PMF value for k
-        """
+        """Calculate PMF value for k successes."""
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
 
+        e = 2.718281828459045
         factorial = 1
         for i in range(1, k + 1):
             factorial *= i
 
-        e = 2.718281828459045
         return (e ** -self.lambtha) * (self.lambtha ** k) / factorial
 
     def cdf(self, k):
-        """Calculate CDF value for k successes.
-
-        Args:
-            k: Number of successes
-
-        Returns:
-            CDF value for k
-        """
+        """Calculate CDF value for k successes."""
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
 
+        e = 2.718281828459045
         cdf_value = 0
         for i in range(k + 1):
-            fact = 1
+            factorial = 1
             for j in range(1, i + 1):
-                fact *= j
-            e = 2.718281828459045
-            cdf_value += (e ** -self.lambtha) * (self.lambtha ** i) / fact
+                factorial *= j
+            cdf_value += (self.lambtha ** i) / factorial
 
-        return cdf_value
+        return (e ** -self.lambtha) * cdf_value
