ef5cf3c5963db80b87a288e82296c2a2b6efe118
Rafael <12767@holbertonstudents.com>
2026-02-14 22:01:20 +0000

    Add Binomial class

---
+++ math/probability/binomial.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Binomial distribution class."""
+
+
+class Binomial:
+    """Represents a binomial distribution."""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Initialize binomial distribution.
+
+        Args:
+            data: List of data to estimate the distribution
+            n: Number of Bernoulli trials
+            p: Probability of a success
+
+        Raises:
+            ValueError: If n is not positive or p is not valid probability
+            TypeError: If data is not a list
+        """
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = float(sum(data) / len(data))
+            variance = 0
+            for x in data:
+                variance += (x - mean) ** 2
+            variance /= len(data)
+
+            p = 1 - (variance / mean)
+            self.n = int(round(mean / p))
+            self.p = float(mean / self.n)
