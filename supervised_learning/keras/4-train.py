8c7570e96c6a18a0a2c7ef511cddb6583a64e26a
Rafael <12767@holbertonstudents.com>
2026-04-02 20:00:45 +0000

    Add train_model function using Keras fit

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module to train a Keras model."""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent.
+    network: the model to train.
+    data: numpy.ndarray of shape (m, nx) containing the input data.
+    labels: one-hot numpy.ndarray of shape (m, classes) containing labels.
+    batch_size: size of the batch used for mini-batch gradient descent.
+    epochs: number of passes through data for mini-batch gradient descent.
+    verbose: boolean that determines if output should be printed.
+    shuffle: boolean that determines whether to shuffle batches every epoch.
+    Returns: the History object generated after training the model.
+    """
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return history
