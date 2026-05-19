8b0a5e858a4a625612ceb5fd088d8d443eeadd6d
Rafael <12767@holbertonstudents.com>
2026-05-19 16:04:19 +0000

    Add Task 1: Implement VGG19 model loading with AveragePooling

---
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""Module for Neural Style Transfer."""
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    """Class to perform tasks for neural style transfer."""
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        """Class constructor for NST."""
+        if (not isinstance(style_image, np.ndarray) or
+                len(style_image.shape) != 3 or style_image.shape[2] != 3):
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+
+        if (not isinstance(content_image, np.ndarray) or
+                len(content_image.shape) != 3 or content_image.shape[2] != 3):
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+
+        if type(alpha) not in [int, float] or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+
+        if type(beta) not in [int, float] or beta < 0:
+            raise TypeError("beta must be a non-negative number")
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+        self.load_model()
+
+    @staticmethod
+    def scale_image(image):
+        """Rescales an image to max 512 pixels and [0, 1] range."""
+        if (not isinstance(image, np.ndarray) or
+                len(image.shape) != 3 or image.shape[2] != 3):
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
+
+        h, w, _ = image.shape
+        max_dim = 512
+        scale = max_dim / max(h, w)
+        new_shape = (int(h * scale), int(w * scale))
+
+        image = tf.convert_to_tensor(image, dtype=tf.float32)
+        image = tf.expand_dims(image, axis=0)
+        image = tf.image.resize(image, new_shape, method='bicubic')
+        image = image / 255.0
+        image = tf.clip_by_value(image, 0.0, 1.0)
+
+        return image
+
+    def load_model(self):
+        """Creates the model used to calculate cost."""
+        vgg = tf.keras.applications.VGG19(include_top=False,
+                                          weights='imagenet')
+        vgg.trainable = False
+
+        inputs = tf.keras.Input(shape=(None, None, 3), name='input_1')
+        x = inputs
+        outputs_dict = {}
+
+        for layer in vgg.layers[1:]:
+            if isinstance(layer, tf.keras.layers.MaxPooling2D):
+                x = tf.keras.layers.AveragePooling2D(
+                    pool_size=layer.pool_size,
+                    strides=layer.strides,
+                    padding=layer.padding,
+                    name=layer.name)(x)
+            else:
+                x = layer(x)
+
+            outputs_dict[layer.name] = x
+
+        outputs = [outputs_dict[name] for name in self.style_layers]
+        outputs.append(outputs_dict[self.content_layer])
+
+        model = tf.keras.Model(inputs=inputs, outputs=outputs)
+        model.trainable = False
+        self.model = model
