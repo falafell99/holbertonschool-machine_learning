3357e96ee657d1aa10a6efa30831bf861be000f3
Rafael <12767@holbertonstudents.com>
2026-05-21 07:59:02 +0000

    Fix Task 4: Resolve pycodestyle E501 line too long errors

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -13,48 +13,41 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
     D, P, betas, H_target = P_init(X, perplexity)
 
     for i in range(n):
-        # Исключаем расстояние от точки до самой себя (диагональ)
+        # Exclude the point's distance to itself
         Di = np.concatenate((D[i, :i], D[i, i + 1:]))
 
         beta = betas[i, 0]
         beta_min = None
         beta_max = None
 
-        # Вычисляем начальную энтропию
         Hi, Pi = HP(Di, beta)
         H_diff = Hi - H_target
 
-        # Бинарный поиск для нахождения правильного значения beta
+        # Binary search for beta
         while np.abs(H_diff) > tol:
             if H_diff > 0:
-                # Энтропия слишком высока -> разброс слишком велик -> нужно сузить радиус
-                # Увеличение beta уменьшает радиус (дисперсию)
+                # Entropy too high -> decrease variance -> increase beta
                 beta_min = beta
                 if beta_max is None:
                     beta *= 2.0
                 else:
                     beta = (beta + beta_max) / 2.0
             else:
-                # Энтропия слишком низка -> разброс слишком мал -> нужно расширить радиус
-                # Уменьшение beta увеличивает радиус
+                # Entropy too low -> increase variance -> decrease beta
                 beta_max = beta
                 if beta_min is None:
                     beta /= 2.0
                 else:
                     beta = (beta + beta_min) / 2.0
 
-            # Пересчитываем с новым beta
             Hi, Pi = HP(Di, beta)
             H_diff = Hi - H_target
 
-        # Сохраняем найденное значение beta (опционально, но полезно для понимания)
         betas[i, 0] = beta
-
-        # Вставляем вероятности обратно в матрицу P (пропуская диагональ)
         P[i, :i] = Pi[:i]
         P[i, i + 1:] = Pi[i:]
 
-    # Вычисление симметричных вероятностей по формуле t-SNE: (P + P.T) / (2 * n)
+    # Calculate symmetric P affinities
     P = (P + P.T) / (2 * n)
 
     return P
