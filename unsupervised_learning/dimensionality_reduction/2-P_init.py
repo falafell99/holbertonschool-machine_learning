7c8b10565566f7a61b3e547f0a88c647086311e6
Rafael <12767@holbertonstudents.com>
2026-05-21 07:27:00 +0000

    Fix Task 2: Resolve PEP8 E501 line too long error

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -4,16 +4,11 @@ import numpy as np
 
 
 def P_init(X, perplexity):
-    """
-    Initializes all variables required to calculate the P affinities in t-SNE.
-    """
+    """Initializes variables to calculate the P affinities in t-SNE"""
     n, d = X.shape
 
-    # Быстрое вычисление квадрата попарных расстояний (a-b)^2 = a^2 - 2ab + b^2
     sum_X = np.sum(np.square(X), axis=1)
     D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
-
-    # Из-за ошибок округления чисел с плавающей точкой диагональ может не быть ровно 0
     np.fill_diagonal(D, 0)
 
     P = np.zeros((n, n))
