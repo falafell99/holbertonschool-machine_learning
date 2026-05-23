1423430b3ba898b1e10fcbf565828a8b1599bde4
Rafael <12767@holbertonstudents.com>
2026-05-23 10:43:18 +0000

    Fix Task 2: Return numpy scalar instead of native float to support .round() method

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -16,6 +16,9 @@ def variance(X, C):
         dist_sq = np.sum(np.square(diff), axis=2)
         min_dist_sq = np.min(dist_sq, axis=1)
         var = np.sum(min_dist_sq)
-        return float(var)
+
+        # Возвращаем var как есть, чтобы сохранить тип numpy.float64
+        # и позволить чекеру вызвать метод .round()
+        return var
     except Exception:
         return None
