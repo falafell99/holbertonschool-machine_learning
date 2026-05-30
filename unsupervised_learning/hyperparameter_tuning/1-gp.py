b193f692b305326233071c013a1fdd521e97144d
Rafael <12767@holbertonstudents.com>
2026-05-30 08:04:24 +0000

    Fix Task 1: Remove long comments to resolve pycodestyle E501 errors

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -29,27 +29,14 @@ class GaussianProcess:
         """
         Predicts the mean and variance of points in a GP.
         """
-        # 1. Вычисляем ковариационные матрицы между известными (X) и новыми (X_s) точками
         K_s = self.kernel(self.X, X_s)
-
-        # 2. Вычисляем ковариацию новых точек между собой
         K_ss = self.kernel(X_s, X_s)
-
-        # 3. Находим обратную матрицу от нашей базовой ковариационной матрицы K
         K_inv = np.linalg.inv(self.K)
 
-        # 4. Вычисляем математическое ожидание (mu)
-        # Формула: mu = K_s.T * K_inv * Y
         mu = K_s.T.dot(K_inv).dot(self.Y)
-
-        # Сплющиваем матрицу формы (s, 1) в вектор (s,)
         mu = mu.reshape(-1)
 
-        # 5. Вычисляем ковариацию предсказаний
-        # Формула: Cov = K_ss - K_s.T * K_inv * K_s
         cov = K_ss - K_s.T.dot(K_inv).dot(K_s)
-
-        # Нам нужна только дисперсия для каждой точки (диагональ матрицы ковариации)
         sigma = np.diagonal(cov)
 
         return mu, sigma
