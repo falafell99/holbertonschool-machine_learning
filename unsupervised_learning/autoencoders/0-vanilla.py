3f5773738e48426a35aae1448a1c2f4bb157e9c8
Rafael <12767@holbertonstudents.com>
2026-06-07 14:47:03 +0000

    Add vanilla autoencoder

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that contains the autoencoder function."""
+import tensorflow.keras as keras
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    inputs = keras.Input(shape=(input_dims,))
+    encoded = inputs
+    for nodes in hidden_layers:
+        encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = keras.Model(inputs=inputs, outputs=latent)
+
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    decoded = latent_inputs
+    for nodes in reversed(hidden_layers):
+        decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
+    outputs = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = keras.Model(inputs=latent_inputs, outputs=outputs)
+
+    auto_outputs = decoder(encoder(inputs))
+    auto = keras.Model(inputs=inputs, outputs=auto_outputs)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    return encoder, decoder, auto
