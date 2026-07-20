7d2354f64e532e95b69ac0eed2c93b010b8481e6
Rafael <12767@holbertonstudents.com>
2026-07-20 12:50:17 +0000

    Fix Task 2: call attention before embedding, drop GRU initial_state

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -32,8 +32,8 @@ class RNNDecoder(tf.keras.layers.Layer):
             return_state=True,
             recurrent_initializer='glorot_uniform'
         )
-        self.attention = SelfAttention(units)
         self.F = tf.keras.layers.Dense(units=vocab)
+        self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
         """
@@ -51,11 +51,10 @@ class RNNDecoder(tf.keras.layers.Layer):
             s: A tensor of shape (batch, units) containing the new decoder
                 hidden state
         """
-        x = self.embedding(x)
         context, weights = self.attention(s_prev, hidden_states)
-        context_expanded = tf.expand_dims(context, 1)
-        concat_x = tf.concat([context_expanded, x], axis=-1)
-        outputs, s = self.gru(concat_x, initial_state=s_prev)
+        x = self.embedding(x)
+        concat_x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
+        outputs, s = self.gru(concat_x)
         outputs = tf.reshape(outputs, (-1, outputs.shape[2]))
         y = self.F(outputs)
         return y, s
