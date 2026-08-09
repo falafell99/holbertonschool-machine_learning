6414fe5f546a866cd3ac8c4cb92ade3eaa07da52
Rafael <12767@holbertonstudents.com>
2026-08-09 11:19:38 +0000

    Task 1: create the loop

---
+++ supervised_learning/qa_bot/1-loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Script that takes input from the user and responds."""
+
+
+if __name__ == "__main__":
+    exit_words = {"exit", "quit", "goodbye", "bye"}
+    while True:
+        question = input("Q: ")
+        if question.lower() in exit_words:
+            print("A: Goodbye")
+            break
+        print("A:")
