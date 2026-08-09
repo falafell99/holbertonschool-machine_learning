d6ca3201ace4c2fafb1c99492e3ce80cc6037d0b
Rafael <12767@holbertonstudents.com>
2026-08-09 11:20:45 +0000

    Task 2: answer loop

---
+++ supervised_learning/qa_bot/2-qa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that contains the answer_loop function."""
+question_answer = __import__('0-qa').question_answer
+
+
+def answer_loop(reference):
+    """
+    Answers questions from a reference text in a loop.
+
+    Args:
+        reference (str): the reference text
+    """
+    exit_words = {"exit", "quit", "goodbye", "bye"}
+    while True:
+        question = input("Q: ")
+        if question.lower() in exit_words:
+            print("A: Goodbye")
+            break
+        answer = question_answer(question, reference)
+        if answer is None or not answer.strip():
+            print("A: Sorry, I do not understand your question.")
+        else:
+            print("A: {}".format(answer))
